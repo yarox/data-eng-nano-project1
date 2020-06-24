8bcab53fe71e756e7c2f409a623a9a6f765d9d03
Adrian Martin <adrianmartinparra@gmail.com>
2020-06-24 16:37:48 +0100

    Implement the ETL process

--- services/jupyter/src/etl.py
+++ services/jupyter/src/etl.py
@@ -1,43 +1,61 @@
-import os
-import glob
-import psycopg2
 import pandas as pd
+import numpy as np
+import psycopg2
+import glob
+import os
+
 from sql_queries import *
 
 
+def get_files(filepath):
+    glob_path = os.path.join(filepath,'**/*.json')
+    return glob.glob(glob_path, recursive=True)
+
+
 def process_song_file(cur, filepath):
-    # open song file
-    df = 
+    # open song file and replace blank data with NaNs
+    df = pd.read_json(filepath, lines=True)
+    df.replace(r'^\s*$', np.nan, regex=True, inplace=True)
 
     # insert song record
-    song_data = 
+    song_columns = ['song_id', 'title', 'artist_id', 'year', 'duration']
+    song_data = df[song_columns].values[0].tolist()
     cur.execute(song_table_insert, song_data)
-    
+
     # insert artist record
-    artist_data = 
+    artist_columns = ['artist_id', 'artist_name', 'artist_location',
+        'artist_latitude', 'artist_longitude']
+
+    artist_data = df[artist_columns].values[0].tolist()
     cur.execute(artist_table_insert, artist_data)
 
 
 def process_log_file(cur, filepath):
     # open log file
-    df = 
+    df = pd.read_json(filepath, lines=True)
 
-    # filter by NextSong action
-    df = 
+    # filter by NextSong action and replace blank data with NaNs
+    df = df[df.page == 'NextSong']
+    df.replace(r'^\s*$', np.nan, regex=True, inplace=True)
 
     # convert timestamp column to datetime
-    t = 
-    
+    t = pd.to_datetime(df['ts'], unit='ms')
+
     # insert time data records
-    time_data = 
-    column_labels = 
-    time_df = 
+    time_data = [df.ts.values, t.dt.hour.values, t.dt.day.values,
+        t.dt.weekofyear.values, t.dt.month.values, t.dt.year.values,
+        t.dt.weekday.values]
+
+    column_labels = ['start_time', 'hour', 'day', 'week', 'month', 'year',
+        'weekday']
+
+    time_df = pd.DataFrame(dict(zip(column_labels, time_data)))
 
     for i, row in time_df.iterrows():
         cur.execute(time_table_insert, list(row))
 
     # load user table
-    user_df = 
+    user_df = df[['userId', 'firstName', 'lastName', 'gender', 'level']]
 
     # insert user records
     for i, row in user_df.iterrows():
@@ -45,28 +63,26 @@ def process_log_file(cur, filepath):
 
     # insert songplay records
     for index, row in df.iterrows():
-        
+
         # get songid and artistid from song and artist tables
         cur.execute(song_select, (row.song, row.artist, row.length))
-        results = cur.fetchone()
-        
-        if results:
-            songid, artistid = results
+        result = cur.fetchone()
+
+        if result:
+            songid, artistid = result
         else:
             songid, artistid = None, None
 
         # insert songplay record
-        songplay_data = 
+        songplay_data = [row.ts, row.userId, row.level, songid, artistid,
+            row.sessionId, row.location, row.userAgent]
+
         cur.execute(songplay_table_insert, songplay_data)
 
 
 def process_data(cur, conn, filepath, func):
     # get all files matching extension from directory
-    all_files = []
-    for root, dirs, files in os.walk(filepath):
-        files = glob.glob(os.path.join(root,'*.json'))
-        for f in files :
-            all_files.append(os.path.abspath(f))
+    all_files = get_files(filepath)
 
     # get total number of files found
     num_files = len(all_files)
@@ -80,7 +96,7 @@ def process_data(cur, conn, filepath, func):
 
 
 def main():
-    conn = psycopg2.connect("host=127.0.0.1 dbname=sparkifydb user=student password=student")
+    conn = psycopg2.connect("host=postgres dbname=sparkifydb user=student password=student")
     cur = conn.cursor()
 
     process_data(cur, conn, filepath='data/song_data', func=process_song_file)
