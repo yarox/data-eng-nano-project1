a8c66ef457987de9b8b261c8da3f6b86a8f1a31f
Adrian Martin <adrianmartinparra@gmail.com>
2020-06-24 16:39:49 +0100

    Fix missing commas

--- services/jupyter/src/sql_queries.py
+++ services/jupyter/src/sql_queries.py
@@ -106,10 +106,10 @@ song_table_insert = ("""
 
 artist_table_insert = ("""
     INSERT INTO artists (
-        artist_id
-        name
-        location
-        latitude
+        artist_id,
+        name,
+        location,
+        latitude,
         longitude
     )
     VALUES (%s, %s, %s, %s, %s)
@@ -119,12 +119,12 @@ artist_table_insert = ("""
 
 time_table_insert = ("""
     INSERT INTO time (
-        start_time
-        hour
-        day
-        week
-        month
-        year
+        start_time,
+        hour,
+        day,
+        week,
+        month,
+        year,
         weekday
     )
     VALUES (%s, %s, %s, %s, %s, %s, %s)
