ad3f6e1693e240402908de3681d42550b3a8975b
Adrian Martin <adrianmartinparra@gmail.com>
2020-06-24 15:17:01 +0100

    Write DROP statements to drop each table

--- services/jupyter/src/sql_queries.py
+++ services/jupyter/src/sql_queries.py
@@ -1,10 +1,10 @@
 # DROP TABLES
 
-songplay_table_drop = ""
-user_table_drop = ""
-song_table_drop = ""
-artist_table_drop = ""
-time_table_drop = ""
+songplay_table_drop = "DROP TABLE IF EXISTS songplays;"
+user_table_drop = "DROP TABLE IF EXISTS users;"
+song_table_drop = "DROP TABLE IF EXISTS songs;"
+artist_table_drop = "DROP TABLE IF EXISTS artists;"
+time_table_drop = "DROP TABLE IF EXISTS time;"
 
 # CREATE TABLES
 
